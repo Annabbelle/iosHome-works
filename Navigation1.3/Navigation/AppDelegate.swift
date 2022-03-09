6d94a7851e5f985932c464484607cb630d3c9092
Annabbelle <chupirchuksolomiya@gmail.com>
2022-03-09 22:02:53 +0100

    добавила 1.3

---
+++ Navigation1.3/Navigation/AppDelegate.swift
@@ -0,0 +1,39 @@
+
+import UIKit
+
+@main
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let tabBarController = UITabBarController()
+        
+        let feedViewController = FeedViewController()
+        feedViewController.view.backgroundColor = UIColor.white
+        let feedNavigationController = UINavigationController(rootViewController: feedViewController)
+        
+        let profileViewController = ProfileViewController()
+        profileViewController.view.backgroundColor = UIColor.white
+        let profileNavigationController = UINavigationController(rootViewController: profileViewController)
+        
+        feedNavigationController.tabBarItem = UITabBarItem(title: "Лента", image: UIImage(named: "Feed"), selectedImage: UIImage(named: "SelectedFeed"))
+        feedNavigationController.navigationBar.titleTextAttributes = [ .foregroundColor: UIColor.white]
+       
+        profileNavigationController.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(named: "Profile"), selectedImage: UIImage(named: "SelectedProfile"))
+        profileNavigationController.navigationBar.titleTextAttributes = [ .foregroundColor: UIColor.white]
+        
+        tabBarController.viewControllers = [feedNavigationController, profileNavigationController]
+        tabBarController.tabBar.isHidden = false
+        
+        self.window?.rootViewController = tabBarController
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+}
---
+++ Navigation1.3/Navigation/PostViewController.swift
@@ -0,0 +1,41 @@
+
+import UIKit
+
+struct Post {
+    var title: String
+}
+
+class PostViewController: UIViewController {
+
+    var post: Post?
+    let infoViewController: InfoViewController
+    
+    init() {
+        infoViewController = InfoViewController()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let infoBarButtonItem = UIBarButtonItem(title: "Инфо", style: .plain, target: self, action: #selector(showInfo))
+        self.navigationItem.rightBarButtonItem  = infoBarButtonItem
+                
+        view.backgroundColor = UIColor.lightGray
+        
+        if let thisPost = post {
+            title = thisPost.title
+        }
+               
+    }
+    
+    @objc func showInfo() {
+        infoViewController.title = title
+        present(infoViewController, animated: true, completion: nil)
+    }
+
+}
